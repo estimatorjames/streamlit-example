997e184ae7b94e5c2a36d80b13e34e9d291b4ee8
James <84405751+estimatorjames@users.noreply.github.com>
2023-05-28 19:50:22 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,31 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+from my_project.langchain_agent import LangChainAgent  # Import your LangChain agent
 
-"""
-# Welcome to Streamlit!
+class ChatApp:
+    def __init__(self):
+        self.agent = LangChainAgent()
+        self.chat_history = []
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+    def add_message(self, user, message):
+        self.chat_history.append({
+            'user': user,
+            'message': message
+        })
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+    def chat(self):
+        st.title('Chat with LangChain Agent')
+        
+        st.markdown("## Chat History")
+        for chat in self.chat_history:
+            st.markdown(f"**{chat['user']}**: {chat['message']}")
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+        user_input = st.text_input("Enter your message:")
+        if st.button('Send'):
+            self.add_message('You', user_input)
+            response = self.agent.respond(user_input)  # Get the response from your agent
+            self.add_message('LangChain Agent', response)
+            
 
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+if __name__ == "__main__":
+    chat_app = ChatApp()
+    chat_app.chat()
